292e1af486bad1609f578b778d885f2c8a08c2b0
Shrinkhalakumari <149521671+natashabt@users.noreply.github.com>
2026-06-13 09:16:08 +0530

    Add files via upload

---
+++ DataDrivenTestingUsingApache-POI/DataDrivenTestDemo/Helper/BrowserFactory4.java
@@ -0,0 +1,29 @@
+package com.Helper;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserFactory4 {
+	
+static WebDriver driver;
+	
+	public static WebDriver BrowserOptions4(String browserName, String url) {
+		if(browserName.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		}
+		else if (browserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		else if (browserName.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		}
+		
+		driver.manage().window().maximize();
+		driver.get(url);
+		
+		return driver;
+	}
+
+}
